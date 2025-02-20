657e151dd73c0b7d2148c1b7bf5c6cf19de787c2
21121a1218 <calveenstephene@gmail.com>
2025-02-20 16:08:28 +0530

    Update testcase2.java

--- testcases/testcase2.java
+++ testcases/testcase2.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class testcase2 {
@@ -29,11 +30,11 @@ public class testcase2 {
             // Select check-in date
             WebElement date = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='fcd9eec8fb d3cfe3ade8 d24fc26e73 c696a7d242']//*[name()='svg']")));
             date.click();
-            WebElement checkIn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@aria-label='20 January 2025']")));
+            WebElement checkIn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@aria-label='23 February 2025']")));
             checkIn.click();
 
             // Select check-out date
-            WebElement checkOut = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@aria-label='25 January 2025']")));
+            WebElement checkOut = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@aria-label='25 February 2025']")));
             checkOut.click();
 
             // Adjust guest count
@@ -52,7 +53,7 @@ public class testcase2 {
             System.out.println("Search results:");
             for (int i = 0; i < Math.min(10, searchResults.size()); i++) {
                 System.out.println(searchResults.get(i).getText());
-                if (searchResults.get(i).getText().contains("Treebo The Qasr")) {
+                if (i==0) {
                     searchResults.get(i).click();
                     break;
                 }
@@ -70,10 +71,45 @@ public class testcase2 {
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", reserve);
             reserve.click();
             // Wait for the "I’ll reserve" button and click it
-            WebElement reserveButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='b_tt_holder_1']")));
-            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", reserveButton);
+            WebElement reserveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"hp_book_now_button\"]/span[2]")));
             reserveButton.click();
-            System.out.println("Reserved successfully.");
+            // Fill booking details
+            WebElement firstname = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id=':rq:']")));
+            firstname.sendKeys("Calveen");
+
+            WebElement lastname = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id=':rr:']")));
+            lastname.sendKeys("Stephene");
+
+            WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id=':rs:']")));
+            email.sendKeys("vk@gmail.com");
+
+            Select countrySelect = new Select(wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@id=':r12:']"))));
+            countrySelect.selectByVisibleText("India");
+
+           WebElement code=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[normalize-space()='Albania +355']")));
+           code.click();           
+            WebElement number = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\":r14:\"]")));
+            number.sendKeys("6300370135");
+
+            WebElement confirmation = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='a53cbfa6de ac9267e216 d8eb520c4e']")));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", confirmation);
+
+            WebElement bookingFor = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(text(),'Booking is for someone else')]")));
+            bookingFor.click();
+
+            WebElement fullname = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='guest_name_852765801_400593686_1_2_0_35441']")));
+            fullname.sendKeys("Calveen Stephene");
+
+            // Print booking info
+            List<WebElement> info = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//form[@name='bookForm']//div//section")));
+            System.out.println("Booking Info:");
+            for (WebElement detail : info) {
+                System.out.println(detail.getText());
+            }
+
+            WebElement nextButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='book']")));
+            nextButton.click();
+
 
         } catch (Exception e) {
             System.err.println("An error occurred: " + e.getMessage());
@@ -87,3 +123,4 @@ public class testcase2 {
         selectAHotel();
     }
 }
+
